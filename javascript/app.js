de48d9ae606a85ac817f8864d388a4b74ec02fda
Billie Heidelberg <billie.heidelberg.jr@gmail.com>
2018-02-16 12:58:43 -0800

    fixes display notes function to be dynamic to each toDo

--- javascript/app.js
+++ javascript/app.js
@@ -1,8 +1,3 @@
-// var toDo = new Object(),
-// 	toDos = [],
-// 	list = document.getElementById('list'),
-// 	item = document.getElementById("input");
-
 var	toDos = [],
 	list = document.getElementById('list'),
 	item = document.getElementById("input");
@@ -11,7 +6,7 @@ var	toDos = [],
 // creates the toDo object 
 function ToDo(item,notes){
 	this.item = item;
-	this.notes = notes;
+	this.notes = [];
 }
 
 // grab toDO from input and trigor display function
@@ -49,19 +44,9 @@ function deleteTodo(ev) {
 	// text content array
 	var deleteMe = del.textContent.split(" ")
 	var holder = [];
-
+	console.log(del)
 //   trims dom li text content string down to just the text
-	function cutString() {
-		for(i=0;i<deleteMe.length;i++){
-				if(deleteMe[i] != ""){
-	 		holder.push(deleteMe[i])
-	 	} if(deleteMe[i] == ""){
-	 		return true
-	 	}
-		}
-	}
-
-	cutString()
+	cutString(deleteMe,holder)
 
 
 		toDos = toDos.filter(function(item) {
@@ -72,43 +57,84 @@ function deleteTodo(ev) {
 	list.removeChild(del)
 }
 
+function cutString(array,holder) {
+	var deleteMe = array;
+	var holder = holder;
+	for(i=0;i<deleteMe.length;i++){
+			if(deleteMe[i] != ""){
+ 		holder.push(deleteMe[i])
+ 	} if(deleteMe[i] == ""){
+ 		return true
+ 	}
+	}
+}
+
 
 function finishTodo(ev) {
 
 	ev.srcElement.parentNode.parentNode.className == "done"? ev.srcElement.parentNode.parentNode.className= "" : ev.srcElement.parentNode.parentNode.className= "done"
 }
 
-	
-
 function expand(ev) {
 	var box = ev.srcElement.nextSibling;
 	box.innerHTML == " " ? box.innerHTML = "<button type='button' id= '" +  box.id + "' onclick='deleteTodo(event)''>x</button>" + " <button type='button' id= '" +  box.id + "' onclick='finishTodo(event)''>&#10004</button>" + "<button type='button' id= '" +  box.id + "' onclick='info(event)''>&#10597</button>" : box.innerHTML = " " ;
+
+}
+ 
+function info(ev) {
+ var notes = document.getElementsByClassName('notes')[ev.srcElement.id];
+  
+    if(notes.innerHTML == " "){
+     notes.innerHTML = "<input id='notes"+ ev.srcElement.id +"'>" + "<button onclick=addNote(event)>poop here</button><br> <ul id='addedNotes" + ev.srcElement.id + "'><ul>"
+ 	}else{
+ 		notes.innerHTML = " ";
+ 	}
+}
+
+function addNote(ev){
+	//input from drop down add notes menu
+	var notesI = ev.srcElement.parentNode.firstChild.value;
+	var notesID = ev.srcElement.parentNode.firstChild.id;
+	// parent todo div
+	var toDo = ev.srcElement.parentNode.parentNode.firstChild;
+	// parent todo div split to parse text
+	var compare = toDo.textContent.split(" ");
+	// holder for custom cut string function
+	var holder = [];
+
+	cutString(compare,holder)
+
+	console.log(notesID)
+
+	for(i=0; i<toDos.length; i++) {
+		if(holder.join("") == toDos[i].item){
+			toDos[i].notes.push(notesI)
+		}
+
+
+		var spot = notesID.split("").filter(function(spot) {
+			if(spot >= 0){
+				return spot
+			}
+		})
+		console.log(spot)
+
+		displayNotes(notesI,parseInt(spot))
+
+	}
 }
 
 
+function displayNotes(note,notesID) {
+	var newLI = document.createElement('li');
+	var note = note;
+	var notesID = notesID;
+	var noteContainer = document.getElementById('addedNotes' + notesID);
 
- 
+	for(i=0;i<toDos.length;i++){
 
-// function info(ev) {
-//  var notes = document.getElementsByClassName('notes')[ev.srcElement.id],
-//      ul = document.createElement('ul');
-//      li = document.createElement('li')
-//      li.innerHTML ="<p>test</p>"
-//     ul.append(li)
-//     console.log(notes)
-
-  
-
-//     if(notes.innerHTML == " "){
-//      notes.innerHTML = "<input>" + "<button onclick=alert('poop')>poop here</button>"
-//  	}else{
-//  		notes.innerHTML = " ";
-//  	}
-//     // if(notes.innerHTML.includes('ul')) {
-//     //     console.log('found');
-//     //     var ul_ref = notes.firstChild;
-//     //     notes.append(li);
-//     // } else {
-//     //     notes.innerHTML = "<ul>"+ul.innerHTML+"</ul>"
-//     // }
-// }
+		toDos[i].notes ? newLI.innerHTML = note : console.log("fail") ;
+		noteContainer.append(newLI)
+	}
+	
+}
